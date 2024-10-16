88a346c777fc9a4cc05ee349f321417b03fa2eae
Patrick <patrick@p15r.net>
2024-10-16 20:13:59 +0200

    feat: Tresorit launcher script

---
+++ create_tresorit_launcher.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+err_report() {
+    echo "Error on line $(caller)" >&2
+    awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),$0 }' L=$1 $0
+}
+trap 'err_report $LINENO' ERR
+
+err_exit() {
+    printf "\n⚠️ $1\n\nExiting.\n"
+    exit 1
+}
+
+out_path="/result/bin/tresorit-fhs"
+tresorit_launcher_file="tresorit_launcher.sh"
+self_path="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+out_path_c="${self_path}${out_path}"
+
+if ! [ -h "${out_path_c}" ]; then
+    err_exit "The output of \"nix build\" could not be found at \"${out_path:1}\"."
+fi
+
+printf "Creating Tresorit launcher...\n"
+tresorit_fhs_shell=$(readlink -f ${out_path_c})
+cat > "${tresorit_launcher_file}" <<EOF
+printf "Starting Tresorit within FHS environment...\n"
+${tresorit_fhs_shell} -c "${HOME}/.local/share/tresorit/tresorit --hidden" &
+printf "Done.\n"
+EOF
+chmod +x "${tresorit_launcher_file}"
+cp "${tresorit_launcher_file}" "${HOME}/.local/share/tresorit/"
+
+if [ -f "${HOME}"/.config/autostart/tresorit.desktop ]; then
+    printf "Removing Tresorit's broken startup config...\n"
+    mv "${HOME}"/.config/autostart/{tresorit.desktop,tresorit.desktop.bk}
+fi
+
+printf "Patching Tresorit startup config...\n"
+if ! [ -f "${HOME}"/.config/autostart/tresorit.desktop.bk ]; then
+    err_exit "Expected to find \"tresorit.desktop.bk\", but it is not present."
+fi
+cp "${HOME}"/.config/autostart/{tresorit.desktop.bk,tresorit-fhs.desktop}
+sed -i \
+    "s|^Name=Tresorit$|Name=Tresorit FHS|" \
+    "${HOME}"/.config/autostart/tresorit-fhs.desktop
+sed -i \
+    "s|^Exec=.*$|Exec=${HOME}/.local/share/tresorit/${tresorit_launcher_file}|" \
+    "${HOME}"/.config/autostart/tresorit-fhs.desktop
+
+printf "Patching Tresorit application config...\n"
+if [ -f "${HOME}"/.local/share/applications/tresorit.desktop ]; then
+    mv "${HOME}"/.local/share/applications/{tresorit.desktop,tresorit.desktop.bk}
+fi
+cp "${HOME}"/.local/share/applications/{tresorit.desktop.bk,tresorit-fhs.desktop}
+sed -i \
+    "s|^Name=Tresorit$|Name=Tresorit FHS|" \
+    "${HOME}"/.local/share/applications/tresorit-fhs.desktop
+sed -i \
+    "s|^Exec=.*$|Exec=${HOME}/.local/share/tresorit/${tresorit_launcher_file}|" \
+    "${HOME}"/.local/share/applications/tresorit-fhs.desktop
+
+printf "Done.\n"
