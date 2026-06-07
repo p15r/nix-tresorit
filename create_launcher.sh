24a447118e3b2df9393d8c34817f7887d6719a8e
Patrick <patrick@g8r.ch>
2026-06-07 19:50:02 +0200

    fix: drop capabilities before running bubblewrap

--- create_launcher.sh
+++ create_launcher.sh
@@ -31,7 +31,8 @@ if [ "${launcher_script}" != "" ]; then
     launch_cmd=${launch_cmd%" &"}
     stop_cmd=$(echo $launch_cmd | sed 's/tresorit \-\-hidden/tresorit-cli stop/')
     printf "Stopping Tresorit FHS...\n"
-    eval "${stop_cmd}"
+    # fails if fhs env is corrupted, continue anyway to link to newly built fhs env
+    eval "${stop_cmd}" || true
 fi
 
 if [ -f "${HOME}/.local/share/tresorit/tresorit_launcher.sh" ]; then
@@ -41,13 +42,22 @@ if [ -f "${HOME}/.local/share/tresorit/tresorit_launcher.sh" ]; then
        ${HOME}/.local/share/applications/tresorit.desktop # restore original file, thus triggering patching script
 fi
 
+# NixOS 26.06 switches the buildFHSEnv backend from buildFHSEnvChroot to
+# bubblewrap, which refuses execution if the user is not root, the setuid bit is
+# not set, and Kernel capabilities are present.
+# Source: https://github.com/containers/bubblewrap/blob/2f55bae38468d0c50cf5df87b1e481e882b63acb/bubblewrap.c#L797
+# This is the case, for example, when using the Ghostty terminal which
+# runs its shells with CAP_SYS_NICE.
+
 printf "Creating Tresorit launcher...\n"
 tresorit_fhs_shell=$(readlink -f "${out_path}")
 cat > "tresorit_fhs_launcher.sh" <<EOF
 #!/usr/bin/env bash
 ${HOME}/.local/share/tresorit/patch.sh
 printf "Starting Tresorit within FHS environment...\n"
-${tresorit_fhs_shell} -c "${HOME}/.local/share/tresorit/tresorit --hidden" > ${HOME}/.local/share/tresorit/fhs.log 2>&1 &
+printf "ℹ️ Dropping all Linux capabilities for bubblewrap.\n"
+setpriv --no-new-privs --inh-caps=-all --ambient-caps=-all -- \
+    ${tresorit_fhs_shell} -c "${HOME}/.local/share/tresorit/tresorit --hidden" > ${HOME}/.local/share/tresorit/fhs.log 2>&1 &
 printf "Done.\n"
 EOF
 chmod +x "tresorit_fhs_launcher.sh"
